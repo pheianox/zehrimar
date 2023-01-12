388008a028182480f200f415fb2c46b661a94e95
pheianox <pheianox@gmail.com>
2023-01-13 00:47:35 +0400

    feat: offline support

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [
+    solidPlugin(),
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      workbox: { globPatterns: ['**/*.{js,css,html,ico,png,svg,json}'] }
+    })
+  ],
   server: {
     port: 3000,
   },
